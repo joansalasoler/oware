fa0745074f37d172b120074c128aa862cef9b98a
Joan Sala <contact@joansala.com>
2021-05-03 10:16:58 +0200

    Reimplements the UCI interpreter using the JLine library

--- src/main/java/com/joansala/cli/ShellCommand.java
+++ src/main/java/com/joansala/cli/ShellCommand.java
@@ -18,12 +18,19 @@ package com.joansala.cli;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.concurrent.Callable;
 import com.google.inject.Inject;
+import org.jline.reader.*;
+import org.jline.terminal.*;
 import picocli.CommandLine.*;
 
+import org.jline.builtins.Completers.TreeCompleter;
+import static org.jline.builtins.Completers.TreeCompleter.node;
+
+import com.joansala.engine.Board;
 import com.joansala.uci.UCIClient;
-import com.joansala.uci.UCIShell;
 
 
 /**
@@ -35,9 +42,6 @@ import com.joansala.uci.UCIShell;
   mixinStandardHelpOptions = true
 )
 public class ShellCommand implements Callable<Integer> {
-
-    /** Injected UCI interpreter instance */
-    private UCIShell shell;
 
     /** UCI client instance */
     private UCIClient client;
@@ -54,9 +58,8 @@ public class ShellCommand implements Callable<Integer> {
     /**
      * Creates a new service.
      */
-    @Inject public ShellCommand(UCIShell shell) {
-        this.client = shell.getUCIClient();
-        this.shell = shell;
+    @Inject public ShellCommand(UCIClient client) {
+        this.client = client;
     }
 
 
@@ -65,7 +68,133 @@ public class ShellCommand implements Callable<Integer> {
      */
     @Override public Integer call() throws Exception {
         client.setService(service);
-        shell.start();
+        runInterpreter();
         return 0;
     }
+
+
+    /**
+     * Runs the UCI interpreter while the client is running.
+     */
+    public void runInterpreter() throws IOException {
+        LineReader reader = newLineReader();
+        PrintWriter writer = reader.getTerminal().writer();
+        Board board = null;
+
+        printWelcome(writer);
+
+        while (client.isRunning()) {
+            try {
+                if (!client.getBoard().equals(board)) {
+                    board = client.getBoard();
+                    printBoard(writer);
+                }
+
+                client.send(nextCommand(reader));
+
+                while (!client.isReady() || !client.isUCIReady()) {
+                    printResponse(writer, client.receive());
+                }
+
+                if (client.hasTimeLimit()) {
+                    while (client.isPondering() || client.isThinking()) {
+                        printResponse(writer, client.receive());
+                    }
+                }
+            } catch (Exception e) {
+                writer.format("Error: %s%n", e.getMessage());
+            }
+        }
+    }
+
+
+    /**
+     * Prints the current board of the UCI client.
+     *
+     * @param writer    Terminal writer
+     */
+    private void printBoard(PrintWriter writer) {
+        writer.println(client.getBoard());
+        writer.flush();
+    }
+
+
+    /**
+     * Prints an engine message to a terminal writer.
+     *
+     * @param writer    Terminal writer
+     * @param message   Engine message
+     */
+    private void printResponse(PrintWriter writer, String message) {
+        writer.format("< %s%n", message);
+        writer.flush();
+    }
+
+
+    /**
+     * Prints a welcome message to a terminal writer.
+     *
+     * @param writer    Terminal writer
+     */
+    private void printWelcome(PrintWriter writer) {
+        Package pack = UCIClient.class.getPackage();
+        String version = pack.getImplementationVersion();
+        writer.format("UCI Interpreter %s%n", version);
+        writer.format("Type UCI commands to send them to the engine.%n%n");
+    }
+
+
+    /**
+     * Reads the next command from the given terminal reader.
+     *
+     * @param reader    Terminal reader
+     */
+    private String nextCommand(LineReader reader) {
+        String line = null;
+
+        while (line == null || line.trim().isEmpty()) {
+            line = reader.readLine("uci> ");
+        }
+
+        return line;
+    }
+
+
+    /**
+     * Creates a new terminal instance.
+     *
+     * @return          New terminal
+     */
+    private static Terminal newTerminal() throws IOException {
+        return TerminalBuilder.builder().build();
+    }
+
+
+    /**
+     * Creates a new terminal reader instance.
+     *
+     * @return          New reader
+     */
+    private static LineReader newLineReader() throws IOException {
+        Terminal terminal = newTerminal();
+        LineReaderBuilder builder = LineReaderBuilder.builder();
+        return builder.terminal(terminal).completer(COMPLETER).build();
+    }
+
+
+    /**
+     * Provides keyword completions for the shell.
+     */
+    private static final Completer COMPLETER = new TreeCompleter(
+        node("debug", node("on", "off")),
+        node("go", node("ponder", "infinite", "movetime", "depth")),
+        node("position", node("fen"), node("startpos", node("moves"))),
+        node("setoption", node("name")),
+        node("ucinewgame"),
+        node("isready"),
+        node("ponderhit"),
+        node("quit"),
+        node("stop"),
+        node("uci")
+    );
 }
