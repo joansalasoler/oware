6c61545cd7c8eaf05ed5407e8ded31277a9fa5b3
Joan Sala <contact@joansala.com>
2021-05-18 09:18:23 +0200

    Show train information and limit the expanded nodes

--- src/main/java/com/joansala/cli/TrainCommand.java
+++ src/main/java/com/joansala/cli/TrainCommand.java
@@ -18,6 +18,7 @@ package com.joansala.cli;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.Callable;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -44,6 +45,12 @@ public class TrainCommand implements Callable<Integer> {
     /** Class injector */
     private final Injector injector;
 
+    /** Game board instance */
+    private final Board parser;
+
+    /** Root game state */
+    private final Game root;
+
     @Option(
       names = "--path",
       description = "Database storage folder"
@@ -68,6 +75,13 @@ public class TrainCommand implements Callable<Integer> {
     )
     private long moveTime = Engine.DEFAULT_MOVETIME;
 
+
+    @Option(
+      names = "--nodes",
+      description = "Number of nodes to expand (nodes)"
+    )
+    private long nodeSize = 1000;
+
     @Option(
       names = "--threads",
       description = "Size of the evaluation thread pool"
@@ -80,6 +94,8 @@ public class TrainCommand implements Callable<Integer> {
      */
     @Inject public TrainCommand(Injector injector) {
         this.injector = injector;
+        this.root = injector.getInstance(Game.class);
+        this.parser = injector.getInstance(Board.class);
     }
 
 
@@ -89,9 +105,6 @@ public class TrainCommand implements Callable<Integer> {
     @Override public Integer call() throws Exception {
         final DOEStore store = new DOEStore(path);
         final DOE trainer = new DOE(store, poolSize);
-
-        final Game root = injector.getInstance(Game.class);
-        final Board parser = injector.getInstance(Board.class);
         final Object position = root.position();
         final int turn = root.turn();
 
@@ -100,17 +113,27 @@ public class TrainCommand implements Callable<Integer> {
         trainer.setInfinity(root.infinity());
         trainer.setExplorationBias(bias);
 
-        // Report search information
+        System.out.format("%s%n", formatSetup());
+        System.out.format("Expanding nodes%n%s%n", horizontalRule('-'));
+
+        // Report best variation found so far
 
         trainer.attachConsumer(report -> {
-            int[] moves = report.getVariation();
-            String notation = parser.toAlgebraic(moves);
-            System.out.format("= %d %s%n", store.count(), notation);
+            final long count = store.count();
+            final int centis = report.getScore();
+            final int[] moves = report.getVariation();
+            System.out.format("= %s%n", fromatResult(moves, centis, count));
         });
 
         // Evaluate postions as they arrive
 
+        AtomicInteger nodeCount = new AtomicInteger();
+
         trainer.trainEngine(root, (moves) -> {
+            if (nodeSize < nodeCount.incrementAndGet()) {
+                trainer.abortComputation();
+            }
+
             Negamax engine = injector.getInstance(Negamax.class);
             Game game = injector.getInstance(Game.class);
 
@@ -122,14 +145,87 @@ public class TrainCommand implements Callable<Integer> {
                 game.makeMove(move);
             }
 
-            String notation = parser.toAlgebraic(moves);
-            System.out.format("- %d %s%n", store.count(), notation);
+            final long count = store.count();
+            final int score = engine.computeBestScore(game);
+            final int centis = game.toCentiPawns(score);
+            System.out.format("- %s%n", fromatResult(moves, centis, count));
 
-            return engine.computeBestScore(game);
+            return score;
         });
 
         store.close();
 
         return 0;
+    }
+
+
+    /**
+     * Formats the current engine setting into a string.
+     *
+     * @retun       A string
+     */
+    private String formatSetup() {
+        return String.format(
+            "Training setup%n" +
+            "%s%n" +
+            "Exploration bias:  %33.3f%n" +
+            "Time per move:     %,33d ms%n" +
+            "Depth limit:       %,33d plies%n" +
+            "Thread pool size:  %,33d threads%n" +
+            "Game class:    %45s%n",
+            horizontalRule('-'),
+            bias,
+            moveTime,
+            depth,
+            poolSize,
+            ellipsis(className(root), 44)
+        );
+    }
+
+
+    /**
+     * Formats a computation result into a string.
+     *
+     * @retun       A string
+     */
+    private String fromatResult(int[] moves, int score, long count) {
+        String notation = parser.toAlgebraic(moves);
+        String result = String.format("%d %6d %s", count, score, notation);
+        return ellipsis(result, 57);
+    }
+
+
+    /**
+     * String representation truncated to 60 characters.
+     *
+     * @param o         An object
+     * @param size      Maximum string size
+     * @return          A string
+     */
+    private static String ellipsis(Object o, int size) {
+        final String v = String.valueOf(o);
+        return v.replaceAll("(?<=^.{" + size + "}).*$", "…");
+    }
+
+
+    /**
+     * Returns an horizontal rule of exactly 60 characters.
+     *
+     * @param c         Rule character
+     * @return          A new string
+     */
+    private static String horizontalRule(char c) {
+        return new String(new char[60]).replace('\0', c);
+    }
+
+
+    /**
+     * Class name of an object.
+     *
+     * @param o         An object
+     * @return          Class name
+     */
+    private static String className(Object o) {
+        return o == null ? "-" : o.getClass().getName();
     }
 }
