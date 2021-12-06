73334b74b49c58be7f27eb86cab440efb16a132a
Joan Sala <contact@joansala.com>
2021-12-06 08:49:34 +0100

    Add simple support for multiword bitboards

--- src/main/java/com/joansala/util/bits/BitsetConverter.java
+++ src/main/java/com/joansala/util/bits/BitsetConverter.java
@@ -112,4 +112,72 @@ public class BitsetConverter {
 
         return NOT_FOUND;
     }
+
+
+    /**
+     * Fills a bitboards position array with the information found
+     * on the occupants array.
+     *
+     * @param position      Bitboards array
+     * @param occupants     Occupants array
+     */
+    public Bitset[] toPosition(Bitset[] position, int[][] occupants) {
+        for (int rank = 0; rank < occupants.length; rank++) {
+            int files = occupants[rank].length;
+
+            for (int file = 0; file < files; file++) {
+                int piece = occupants[rank][file];
+
+                if (piece >= 0 && piece < position.length) {
+                    int index = files * rank + file;
+                    position[piece].insert(bits[index]);
+                }
+            }
+        }
+
+        return position;
+    }
+
+
+    /**
+     * Fills an occupants array with the information found on the
+     * bitboards position array.
+     *
+     * @param occupants     Occupants array
+     * @param position      Bitboards array
+     */
+    public int[][] toOccupants(int[][] occupants, Bitset[] position) {
+        for (int rank = 0; rank < occupants.length; rank++) {
+            int files = occupants[rank].length;
+
+            for (int file = 0; file < files; file++) {
+                int index = files * rank + file;
+                int piece = piece(position, index);
+                occupants[rank][file] = piece;
+            }
+        }
+
+        return occupants;
+    }
+
+
+    /**
+     * Identifier of the piece on the given coordinate index.
+     *
+     * @param position      Position bitboards
+     * @param index         Coordinate index
+     *
+     * @return              Piece identifier or {@code NOT_FOUND}
+     */
+    public int piece(Bitset[] position, int index) {
+        final int bit = bits[index];
+
+        for (int piece = 0; piece < position.length; piece++) {
+            if (position[piece].contains(bit)) {
+                return piece;
+            }
+        }
+
+        return NOT_FOUND;
+    }
 }
